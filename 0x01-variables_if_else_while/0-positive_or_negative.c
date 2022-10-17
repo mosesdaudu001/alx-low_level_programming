06c53b1a1bed773edbb55280ebcd79aee36d1469
mosesdaudu001 <mosesdaudu001@gmail.com>
2022-10-17 04:05:53 -0700

    trfrfrfr

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,6 +4,12 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
 int main(void)
 {
 	int n;
