d67f0cbd1378037ce07a3eb7c60bb24e696d6adc
mosesdaudu001 <mosesdaudu001@gmail.com>
2022-10-18 06:38:39 -0700

    errc

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isalpha - Write a function that checks for alphabetic character
+ * @c: is the char to be checked
+ * Return: 1 if c is a letter, lowercase or uppercase, 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
+
